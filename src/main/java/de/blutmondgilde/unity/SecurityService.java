0fd1d5c2e2e36ae2d44ac921545574c4d3bf7895
Christian Raoulis <craoulis@googlemail.com>
2021-11-23 21:58:46 +0100

    add Login check

--- src/main/java/de/blutmondgilde/unity/SecurityService.java
+++ src/main/java/de/blutmondgilde/unity/SecurityService.java
@@ -3,6 +3,8 @@ package de.blutmondgilde.unity;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.server.VaadinServletRequest;
 import de.blutmondgilde.unity.data.DiscordOAuthUser;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
@@ -18,6 +20,11 @@ public class SecurityService {
 
     SecurityService(OAuth2AuthorizedClientService authorizedClientService) {
         this.authorizedClientService = authorizedClientService;
+    }
+
+    public boolean isLoggedIn() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return authentication.isAuthenticated() && !(authentication instanceof AnonymousAuthenticationToken);
     }
 
     public DiscordOAuthUser getAuthenticatedUser() {
